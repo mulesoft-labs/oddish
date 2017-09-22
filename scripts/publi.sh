ab0338008acf235fcfadec8f5a1e3c8b4c6edd59
matiasbargas <matiasbargas@gmail.com>
2017-09-22 16:58:33 -0300

    remove check for npm user

--- scripts/publi.sh
+++ scripts/publi.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
+echo "pwd=$(pwd)"
+
 export DEFAULT_BASE_VERSION
-
 DEFAULT_BASE_VERSION="$(npm view . version)"
-
 if [ -z "$DEFAULT_BASE_VERSION" ]; then
    DEFAULT_BASE_VERSION='1.0.0'
 fi
 
-echo "pwd=$(pwd)"
-
 export GIT_BRANCH
 ${GIT_BRANCH:=$BRANCH_NAME} # Jenkins
 ${GIT_BRANCH:=$TRAVIS_BRANCH} # Travis
+
 if [ -z "${GIT_BRANCH}" ]; then
   GIT_BRANCH=$(git symbolic-ref --short HEAD)
 fi
-
 echo "branch=${GIT_BRANCH}"
 
 export TAG="latest"
@@ -26,41 +24,26 @@ elif [ "${GIT_BRANCH}" = "develop" ]; then
 else
   TAG=${GIT_BRANCH}
 fi
-
-echo "Used target tag=$TAG"
+echo "tag=$TAG"
 
 export LATEST_VERSION
 LATEST_VERSION=$(npm v . "dist-tags.$TAG")
-echo "latest version=$LATEST_VERSION"
-
 if [ "$LATEST_VERSION" = "undefined" ]; then
   LATEST_VERSION="$DEFAULT_BASE_VERSION-$GIT_BRANCH"
-fi
-
-if [ "$LATEST_VERSION" = "" ]; then
+elif [ "$LATEST_VERSION" = "" ]; then
   LATEST_VERSION="$DEFAULT_BASE_VERSION-$GIT_BRANCH"
 fi
 
-export DEVELOP_VERSION_BASE="$LATEST_VERSION"
+echo "base version=$LATEST_VERSION"
 
-echo "base version=$DEVELOP_VERSION_BASE"
+npm version "${LATEST_VERSION}" --force --no-git-tag-version 2> /dev/null 1> /dev/null
+export NEW_VERSION=$(npm version prerelease --force --no-git-tag-version 2> /dev/null)
+echo "publishing branch ${GIT_BRANCH} with version ${NEW_VERSION}"
 
-export NPM_USER
-NPM_USER=$(npm whoami --registry https://npm.mulesoft.com 2> /dev/null)
+set -e # Break the script if it fails
 
-if [ -z "${NPM_USER}" ]; then
-    echo 'NPM_USER NOT VALID FOR @mulesoft REGISTRY. CHECK THE ENV-VAR NPM_TOKEN'
-    exit 1;
+if [ "${TAG}" = "latest" ]; then
+  npm publish
 else
-    npm version "${DEVELOP_VERSION_BASE}" --force --no-git-tag-version 2> /dev/null 1> /dev/null
-    DEVELOP_VERSION_BASE=$(npm version prerelease --force --no-git-tag-version 2> /dev/null)
-    echo "publishing branch ${GIT_BRANCH} with version ${DEVELOP_VERSION_BASE} AS @${NPM_USER}"
-
-    set -e # Break the script if it fails
-
-    if [ "${TAG}" = "latest" ]; then
-      npm publish
-    else
-      npm publish "--tag=$TAG"
-    fi
+  npm publish "--tag=$TAG"
 fi
